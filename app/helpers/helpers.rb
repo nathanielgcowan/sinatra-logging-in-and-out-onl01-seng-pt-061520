4f775e5d23f273f7861f8f64689f76c81dc000a5
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-25 21:53:22 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+    def self.current_user(session)
+        # should accept the session hash as an argument 
+        # should use the user_id from the session hash to find the user in the daabase and return that user.
+        @user = User.find_by_id(session[:user_id])
+    end 
+
+    def is_logged_in?(session)
+        #should accept the session has as an argument 
+        #should use the user_id from the session has to find the user in the database and return that user.
+        !!session[:user_id]
+    end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,2 @@
-
-
+class Users < ActiveRecord::Base 
+end 
